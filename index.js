5e90ef53674fb6d0b0c4055d5d7b05bc54a5dd14
Alperen Berat DURMUŞ <alperen@creditonline.eu>
2023-04-27 22:32:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function myFunction() {
-	console.log('Hello, world!');
+function isPrivateIP(ip) {
+	const octets = ip.split(':').pop().split('.').map(Number);
+	if (octets.length === 4) {
+		if (octets[0] === 10) {
+			return true;
+		} else if (octets[0] === 172 && octets[1] >= 16 && octets[1] <= 31) {
+			return true;
+		} else if (octets[0] === 192 && octets[1] === 168) {
+			return true;
+		} else if (octets[0] === 127) {
+			return true;
+		}
+	} else if (ip === '::1') {
+		return true;
+	}
+
+	return false;
 }
 
 module.exports = {
-	myFunction: myFunction
+	isPrivateIP
 };
